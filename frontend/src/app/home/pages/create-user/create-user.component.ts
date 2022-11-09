41519884316a8985298f39728b02ad11d8ab638f
José Daniel <jdsc2@cin.ufpe.br>
2022-11-09 13:23:06 -0300

    REFACTOR: user type and interface

--- frontend/src/app/home/pages/create-user/create-user.component.ts
+++ frontend/src/app/home/pages/create-user/create-user.component.ts
@@ -2,6 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserService } from 'src/app/shared/services/user.service';
 
+import {
+  civilStateList,
+  gradeList,
+  placeList,
+  religionList,
+  residenceList,
+  salaryList,
+  sexList,
+  yesNo,
+  ListInterface,
+} from '../../types/form';
+
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
@@ -10,117 +22,22 @@ import { UserService } from 'src/app/shared/services/user.service';
 export class CreateUserComponent implements OnInit {
   form: FormGroup;
 
+  formList: ListInterface[][] = [];
   autoResize: boolean = true;
   verify: boolean = false;
-  childrenNumber: number = 0;
-
-  religionList: any[] = [
-    { label: 'Adventista do 7° dia', value: 'Adventista do 7° dia' },
-    { label: 'Assembléia de Deus', value: 'Assembléia de Deus' },
-    { label: 'Batista', value: 'Batista' },
-    { label: 'Brasil para Cristo', value: 'Brasil para Cristo' },
-    { label: 'Budista', value: 'Budista' },
-    { label: 'Candomblé', value: 'Candomblé' },
-    { label: 'Catolicismo', value: 'Catolicismo' },
-    {
-      label: 'Congregação Cristã no Brasil',
-      value: 'Congregação Cristã no Brasil',
-    },
-    { label: 'Cristão', value: 'Cristão' },
-    { label: 'Deus é Amor', value: 'Deus é Amor' },
-    { label: 'Espiritismo', value: 'Espiritismo' },
-    { label: 'Evangelho Quadrangular', value: 'Evangelho Quadrangular' },
-    { label: 'Evangélica', value: 'Evangélica' },
-    { label: 'Hare Krishna', value: 'Hare Krishna' },
-    {
-      label: 'Internacional da Graça de Deus',
-      value: 'Internacional da Graça de Deus',
-    },
-    { label: 'Islamismo', value: 'Islamismo' },
-    { label: 'Judaismo', value: 'Judaismo' },
-    { label: 'Legião da Boa Vontade', value: 'Legião da Boa Vontade' },
-    { label: 'Luterana', value: 'Luterana' },
-    { label: 'Maranata', value: 'Maranata' },
-    { label: 'Messiânica', value: 'Messiânica' },
-    { label: 'Metodista', value: 'Metodista' },
-    { label: 'Mundial do poder de Deus', value: 'Mundial do poder de Deus' },
-    { label: 'Pentecostal', value: 'Pentecostal' },
-    { label: 'Presbiteriana', value: 'Presbiteriana' },
-    { label: 'Protestante', value: 'Protestante' },
-    {
-      label: 'Santos dos Últimos Dias (Mórmon)',
-      value: 'Santos dos Últimos Dias (Mórmon)',
-    },
-    { label: 'Satanismo', value: 'Satanismo' },
-    { label: 'Seicho-No-le', value: 'Seicho-No-le' },
-    { label: 'Testemunha de Jeová', value: 'Testemunha de Jeová' },
-    { label: 'Umbanda', value: 'Umbanda' },
-    {
-      label: 'Universal do Reino de Deus',
-      value: 'Universal do Reino de Deus',
-    },
-    { label: 'Outros', value: 'Outros' },
-    { label: 'Sem religião', value: 'Sem religião' },
-  ];
-
-  residenceList: any[] = [
-    { label: 'Própria Quitada', value: 1 },
-    { label: 'Alugada', value: 2 },
-    { label: 'Cedida', value: 3 },
-    { label: 'Financiada', value: 4 },
-    { label: 'Outro', value: 0 },
-  ];
-
-  yesNo: any[] = [
-    { label: 'Sim', value: true },
-    { label: 'Não', value: false },
-  ];
-
-  placeList: any[] = [
-    { label: 'Alto José Leal', value: 'Alto José Leal' },
-    { label: 'Bela vista 2', value: 'Bela vista 2' },
-    { label: 'Lot. José de Lemos', value: 'Lot. José de Lemos' },
-    { label: 'Lot. Palácio Boa Vista', value: 'Lot. Palácio Boa Vista' },
-    { label: 'Lot. de Baú', value: 'Lot. de Baú' },
-    { label: 'Lot. Vida Nova', value: 'Lot. Vida Nova' },
-    { label: 'Vitória Coqueiros', value: 'Vitória Coqueiros' },
-    { label: 'Vale Verde', value: 'Vale Verde' },
-    { label: 'Outro', value: 'Outro' },
-  ];
-
-  salaryList: any[] = [
-    { label: 'menos de 01 Salário', value: 1 },
-    { label: '01 a 02 Salários', value: 2 },
-    { label: '03 a 05 Salários', value: 3 },
-    { label: 'Mais de 05 Salários', value: 4 },
-  ];
-
-  sexList: any[] = [
-    { label: 'Masculino', value: 'M' },
-    { label: 'Feminino', value: 'F' },
-    { label: 'Outro', value: 'O' },
-  ];
-
-  gradeList: any[] = [
-    { label: 'Superior completo' },
-    { label: 'Superior incompleto' },
-    { label: 'Ensino médio completo' },
-    { label: 'Ensino médio incompleto' },
-    { label: 'Ensino fundamental completo' },
-    { label: 'Ensino fundamental incompleto' },
-    { label: 'Não sei' },
-  ];
-
-  civilStateList: any[] = [
-    { label: 'Solteiro' },
-    { label: 'Casado' },
-    { label: 'Divorciado' },
-    { label: 'Viúvo' },
-    { label: 'União Estável' },
-    { label: 'Não sei' },
-  ];
 
   constructor(private service: UserService, private formBuilder: FormBuilder) {
+    this.formList = [
+      civilStateList,
+      gradeList,
+      placeList,
+      religionList,
+      residenceList,
+      salaryList,
+      sexList,
+      yesNo,
+    ];
+
     this.form = this.formBuilder.group({
       person: this.formBuilder.group({
         name: [null, [Validators.required, Validators.minLength(5)]],
