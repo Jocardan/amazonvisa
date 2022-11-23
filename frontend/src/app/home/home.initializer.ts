38f69091647ca8438641f3fe633b0b010a11ff3b
José Daniel <jdsc2@cin.ufpe.br>
2022-11-22 22:11:48 -0300

    CHORE: Add set profile

--- frontend/src/app/home/home.initializer.ts
+++ frontend/src/app/home/home.initializer.ts
@@ -1,8 +1,18 @@
 import { APP_INITIALIZER } from '@angular/core';
+import { ProfileService } from '../shared/services/profile.service';
 
 import { UserService } from '../shared/services/user.service';
 
-export const homeInitializer = (usersService: UserService) => () => {};
+export const homeInitializer =
+  (usersService: UserService, profileService: ProfileService) => () => {
+    profileService.getProfile().subscribe(async (profile) => {
+      const token = localStorage.getItem('TOKEN');
+      if (!profile && token) {
+        // const profile = await usersService.getUsers()
+        // profileService.setProfile(profile)
+      }
+    });
+  };
 
 export const homeInitializerProvider = {
   provide: APP_INITIALIZER,
