724550e7697398e51ff0a9f59960a3431a32fa40
José Daniel <jdsc2@cin.ufpe.br>
2023-10-13 02:41:24 -0300

    FEAT: Benefits page

---
+++ frontend/src/app/user/types/partner.ts
@@ -0,0 +1,7 @@
+export interface Partner {
+  title: string;
+  benefit: string;
+  phone: number;
+  image: string | undefined;
+  address: string;
+}
--- frontend/src/app/user/user.module.ts
+++ frontend/src/app/user/user.module.ts
@@ -9,10 +9,11 @@ import { UserPageComponent } from './pages/user-page/user-page.component';
 import { CommonModule } from '@angular/common';
 import { InfoPageComponent } from './pages/info-page/info-page.component';
 import { ReportPageComponent } from './pages/report-page/report-page.component';
+import { BenefitsPageComponent } from './pages/benefits-page/benefits-page.component';
 
 @NgModule({
   providers: [UserState, UserFacade, userInitializerProvider],
-  declarations: [LoginComponent, UserPageComponent, InfoPageComponent, ReportPageComponent],
+  declarations: [LoginComponent, UserPageComponent, InfoPageComponent, ReportPageComponent, BenefitsPageComponent],
   imports: [UserRoutingModule, SharedModule, CommonModule],
 })
 export class UserModule {}
