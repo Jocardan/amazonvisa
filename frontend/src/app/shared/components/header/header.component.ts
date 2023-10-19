fd02d39379d0654d5ec4b65b418916364fe640f8
José Daniel <jdsc2@cin.ufpe.br>
2023-10-19 02:10:21 -0300

    FIX: User pages MVP finished

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -12,7 +12,7 @@ export class HeaderComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit(): void {
-    this.url = this.router.url.replace('/', '')
+    this.url = this.router.url.split('/')[1]
   }
 
 }
